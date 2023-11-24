cd47bf69ccd5cf05e55cc2aa2be723a36f0b7aa7
Natalia Gribanova <kara333@mail.ru>
2023-11-24 21:48:34 +0300

    Override magic method __str__ for all classes Add average grades comparsion for classes Student and Lector

--- classes.py
+++ classes.py
@@ -15,6 +15,26 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+    
+    def avg_grades(self):
+        all_grades = []
+        for grade in self.grades.values():
+            all_grades += grade
+        return round(sum(all_grades) / len(all_grades), 1)
+
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за домашние задания: {self.avg_grades()}\n"
+                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
+                f"Завершенные курсы: {', '.join(self.finished_courses)}")
+    
+    def __lt__(self, other):
+        return self.avg_grades() < other.avg_grades()
+    
+    def __eq__(self, other):
+        return self.avg_grades() == other.avg_grades()
+
         
 class Mentor:
     def __init__(self, name, surname):
@@ -27,6 +47,23 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def avg_grades(self):
+        all_grades = []
+        for grade in self.grades.values():
+            all_grades += grade
+        return round(sum(all_grades) / len(all_grades), 1)
+    
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за лекции: {self.avg_grades()}")
+    
+    def __lt__(self, other):
+        return self.avg_grades() < other.avg_grades()
+    
+    def __eq__(self, other):
+        return self.avg_grades() == other.avg_grades()
+
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -36,3 +73,8 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}")
+    
